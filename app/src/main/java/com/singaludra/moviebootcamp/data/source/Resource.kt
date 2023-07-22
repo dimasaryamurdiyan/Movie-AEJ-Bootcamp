43fd725ad7b02ce5d9c2b795813e5f09b2e95bba
dmurdiyan <dmurdiyan@binar.co.id>
2023-07-23 00:48:12 +0700

    fix: move Resource class root directory

--- app/src/main/java/com/singaludra/moviebootcamp/data/source/Resource.kt
+++ app/src/main/java/com/singaludra/moviebootcamp/data/source/Resource.kt
@@ -1,4 +1,4 @@
-package com.singaludra.moviebootcamp.data.source.remote
+package com.singaludra.moviebootcamp.data.source
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
