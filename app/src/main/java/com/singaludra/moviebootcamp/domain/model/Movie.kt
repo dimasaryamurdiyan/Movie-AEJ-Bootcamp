db748528c5e10a79b981a014c3e75cb65734eb9f
dmurdiyan <dmurdiyan@binar.co.id>
2023-07-22 22:44:54 +0700

    feat: add dto model and response model

---
+++ app/src/main/java/com/singaludra/moviebootcamp/domain/model/Movie.kt
@@ -0,0 +1,23 @@
+package com.singaludra.moviebootcamp.domain.model
+import android.os.Parcelable
+
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Movie(
+    val adult: Boolean,
+    val backdropPath: String,
+    val genreIds: List<Int>,
+    val id: Int,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String,
+    val releaseDate: String,
+    val title: String,
+    val video: Boolean,
+    val voteAverage: Double,
+    val voteCount: Int
+) : Parcelable
