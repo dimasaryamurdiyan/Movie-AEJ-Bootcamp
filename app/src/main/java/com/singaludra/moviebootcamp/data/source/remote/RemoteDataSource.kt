8e44350e7158dab0ebf0ea9a583a1b3fd0fc932e
dmurdiyan <dmurdiyan@binar.co.id>
2023-07-23 13:11:37 +0700

    fix: separate exception when fetching data

--- app/src/main/java/com/singaludra/moviebootcamp/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/singaludra/moviebootcamp/data/source/remote/RemoteDataSource.kt
@@ -8,7 +8,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import retrofit2.HttpException
 import retrofit2.http.Query
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -32,10 +34,30 @@ class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
                     emit(ApiResponse.Empty)
                 }
             } catch (e : Exception){
-                emit(ApiResponse.Error(e.toString()))
+                emit(ApiResponse.Error(e.parse()))
                 Log.e("RemoteDataSource", e.toString())
             }
         }.flowOn(Dispatchers.IO)
+    }
+
+    private fun Exception.parse(): String {
+        return when(this){
+            is IOException -> {
+                "You have no connection!"
+            }
+
+            is HttpException -> {
+                if(this.code() == 422){
+                    "Invalid Data"
+                } else {
+                    "Invalid request"
+                }
+            }
+
+            else -> {
+                this.message.toString()
+            }
+        }
     }
 
 
